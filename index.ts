159c2c9b21f5ab753bfcbace4715f37c84b21028
Joshua Byrd <phocks@gmail.com>
2023-11-10 11:40:55 +1000

    Add a few

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import equal from "fast-deep-equal";
+import { equals } from "ramda";
+import { isEqual } from "lodash";
 
 function returnObjects() {
   const object = JSON.stringify({
@@ -14,16 +16,30 @@ function returnObjects() {
 const DATA = returnObjects();
 const ITERATIONS = 10_000_000;
 
-// Time a benchmark for the two objects
+// Time a benchmark for the two objects for JSON.stringify
 console.time("Stringify benchmark");
 for (let i = 0; i < ITERATIONS; i++) {
   JSON.stringify(DATA[0]) === JSON.stringify(DATA[1]);
 }
 console.timeEnd("Stringify benchmark");
 
-// Time a benchmark for the two objects
+// Time a benchmark for the two objects for fast-deep-equal
 console.time("fast-deep-equal benchmark");
 for (let i = 0; i < ITERATIONS; i++) {
   equal(DATA[0], DATA[1]);
 }
 console.timeEnd("fast-deep-equal benchmark");
+
+// Time a benchmark for the two objects for Ramda
+console.time("Ramda benchmark");
+for (let i = 0; i < ITERATIONS; i++) {
+  equals(DATA[0], DATA[1]);
+}
+console.timeEnd("Ramda benchmark");
+
+// Time a benchmark for the two objects for Lodash
+console.time("Lodash benchmark");
+for (let i = 0; i < ITERATIONS; i++) {
+  isEqual(DATA[0], DATA[1]);
+}
+console.timeEnd("Lodash benchmark");
